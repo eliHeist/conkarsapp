70f235bac0b30b23505d887594e2083ed7ab1df2
eliheist <eliyang256@outlook.com>
2023-01-01 19:31:45 +0300

    set up templates and routes for about and contact apps

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def AboutView(request):
+   context = {}
+   template_name = 'about/about-page.html'
+   return render(request, template_name, context)
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ContactView(request):
+   context = {}
+   template_name = 'contact/contact-page.html'
+   return render(request, template_name, context)
