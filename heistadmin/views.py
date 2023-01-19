e60d15bcd8a79943afc28e55334d309a08d8539b
eliheist <eliyang256@outlook.com>
2023-01-19 22:40:01 +0300

    added dynamic navigation list

--- heistadmin/views.py
+++ heistadmin/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 
+from projects.models import Category, Project
+
 # Create your views here.
+registered_models = {Project,Category}
+# create a base context containing slugs for registered models
+base_context = {}
+base_models = []
+for model in registered_models:
+   # insert at the end of the file to maintain order of registered_models
+   base_models.insert(-1, model.__name__)
+# add models list to context
+base_context["models"] = base_models
+
+def registerModels():
+   pass
+
 def defaultView(request):
    template_name = 'heistadmin_base.html'
-   context = {}
+   context = base_context
+   for model in registered_models:
+      # add model to context
+      context[f"{model.__name__}"] = model
+      # insert at the end of the file to maintain order of registered_models
+
+   return render(request, template_name, context)
+
+
+def listView(request, slug):
+   template_name = 'heistadmin_base.html'
+   context = base_context
+   
+
    return render(request, template_name, context)
